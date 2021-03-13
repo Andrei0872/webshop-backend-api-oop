f7e1feac279844d21c8f0386eb2b9c059ab7d936
Andrei Gatej <andreigtj01@gmail.com>
2021-03-14 00:15:32 +0200

    feat(db): use Singleton Pattern

--- db/db.cpp
+++ db/db.cpp
@@ -1,3 +1,20 @@
+#include "../config.h"
+
+using namespace std;
+
 class Database {
-  // TODO: singleton pattern
+  private:
+    Config config;
+
+    // Making sure it remains singleton
+    Database () { cout << "DB\n"; }
+    Database(Database const &);
+    void operator=(Database const &);
+  public:
+
+    static Database& getInstance () {
+      static Database instance;
+
+      return instance;
+    }
 };
