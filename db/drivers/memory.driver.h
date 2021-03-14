579b8749ce6115ecd5eb2f0de8d1af81e231e210
Andrei Gatej <andreigtj01@gmail.com>
2021-03-15 00:01:40 +0200

    feat(db): add `MemoryDriver` basic implementation

--- db/drivers/memory.driver.h
+++ db/drivers/memory.driver.h
@@ -1,13 +1,33 @@
 #pragma once
 
-#include "../../utils/types.h"
+#include "../db.h"
 
 class MemoryDriver : public DBDriver {
   private:
-    map<string, map<string, DBTable<Entity>>> databases;
+
+    // Just some hardcoded data when using this driver
+    void populateDb (Config config) {
+      TablesMap tables;
+
+      // DBTable* productTable = new DBTable("product");
+      // tables["product"] = productTable;
+
+      // DBTable *userTable = new DBTable("user");
+      // tables["user"] = userTable;
+
+      // DBTable *adminTable = new DBTable("admin");
+      // tables["admin"] = adminTable;
+
+      databases[config.getDBName()] = tables;
+    }
 
   public:
-    MemoryDriver(): DBDriver("memory") {
-      
+    MemoryDriver(): DBDriver("@ID_MEMORY_DRIVER") {
+      cout << "MEMORY DRIVER INIT \n";
+    }
+
+    virtual void connect(Config config) {
+      cout << "[MEMORY DRIVER]: CONNECTING \n";
+      populateDb(config);
     }
 };
