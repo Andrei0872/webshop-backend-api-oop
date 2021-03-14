f97799b07d9ad5ce652e35bb8da21cb6d6831342
Andrei Gatej <andreigtj01@gmail.com>
2021-03-15 00:02:15 +0200

    feat: add db initialization in `main.cpp`

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 
+#include "./utils/types.h"
+#include "./components/product/product.entity.h"
+#include "./db/drivers/memory.driver.h"
+#include "./components/user/user.entity.h"
+#include "./components/user/user.repository.h"
+
 using namespace std;
 
 // TODO: 
 // initiate server
 
+class Foo {};
 
 int main () {
-  cout << "hello! \n";
+  MemoryDriver* memoryDriver = new MemoryDriver();
+  Database::getInstance().setDriver(memoryDriver);
+  // Database::getInstance().printDriverId();
+
+  // Normally, this would be an async action
+  // but in case we're just using the MemoryDriver
+  // which does not require any async actions
+  Database::getInstance().connect();
+
+  UserRepository u;
+
+  cout << u.selectAll().size();
 }
