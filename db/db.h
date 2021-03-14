31c3d3a52592283ba04ab8cde5997cddef8113d5
Andrei Gatej <andreigtj01@gmail.com>
2021-03-14 23:59:30 +0200

    feat(db): add base type needed for set-up

---
+++ db/db.h
@@ -0,0 +1,111 @@
+#include "../config.h"
+#include<map>
+#include<string>
+#include <vector>
+
+using namespace std;
+
+class Bar{};
+
+class DBTable {
+  protected:
+    string name;
+    vector<string> columns;
+    vector<Entity> data;
+
+  public:
+    DBTable(string name = ""): name(name) {
+      cout << "TABLE INIT \n";
+    };
+
+    void hello () {
+      cout << "Hello from DBTABLE \n";
+    }
+};
+
+typedef map<string, DBTable*> TablesMap;
+typedef map<string, TablesMap> DatabasesMap;
+
+class DBDriver {
+  private:
+    string ID;
+
+  protected:
+    DatabasesMap databases;
+
+  public:
+    DBDriver(string ID = ""): ID(ID) {};
+
+    TablesMap selectDb(string name) {
+      return databases.at(name);
+    };
+    
+    DBTable* selectTable (TablesMap tables, string tableName) {
+      cout << "CRT TABLE NAME " << tableName << '\n';
+      
+      return tables.at(tableName);
+    }
+
+    string getId () {
+      return ID;
+    }
+    virtual void connect(Config config) {};
+};
+
+class Database {
+  private:
+    Config config;
+    DBDriver* driver;
+    TablesMap crtDb;
+    TablesMap tables;
+
+    Database () { cout << "DB INIT\n"; }
+
+    void setDbBasedOnConfig () {
+      tables = driver->selectDb(config.getDBName());
+    }
+  
+  public:
+
+    static Database& getInstance () {
+      static Database instance;
+
+      return instance;
+    }
+
+    TablesMap getTables () {
+      return tables;
+    }
+
+    void setDriver (DBDriver* newDriver) {
+      driver = newDriver;
+    }
+
+    void printDriverId () {
+      cout << "CURRENT DRIVER ID: " << driver->getId() << "\n";
+    }
+
+    void connect () {
+      driver->connect(config);
+      setDbBasedOnConfig();
+    }
+
+    DBTable* selectTable (string tableName) {
+      return driver->selectTable(crtDb, tableName);
+    }
+};
+
+class Repository {
+  protected:
+    Database db;
+
+  protected:
+  
+    // Add tables to DB _dynamically_, based on entity repositories
+    void setTable (string tableName, DBTable* table) {
+      db.getTables().insert({ tableName, table });
+    }
+  
+  public:
+    Repository (): db(Database::getInstance()) {};
+};
--- utils/types.h
+++ utils/types.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <vector>
 
 using namespace std;
 
@@ -11,35 +12,4 @@ class Serializable {
     virtual string serialize() = 0;
 };
 
-class Entity : public Serializable {};
-
-// template<typename T>
-// class DBTable {
-//   private:
-//     string name;
-//     string *columns;
-//     T* dataList;
-//   public:
-//     DBTable(T entity) {};
-
-//     T* selectAll();
-//     T find ();
-//     bool deleteOne (T item);
-// };
-
-// map<string, DBTable<Entity>> selectDb(string name);
-// DBTable<Entity> selectTable(map<string, DBTable<Entity>> tables);
-
-class DBDriver {
-  private:
-    string ID;
-  public:
-    DBDriver(string ID): ID(ID) {};
-
-    template<typename DB>
-    DB selectDb(string name);
-    template<typename DB, typename T>
-    T selectTable (DB tables);
-
-    virtual void connect(Config c);
-};
+class Entity {};
